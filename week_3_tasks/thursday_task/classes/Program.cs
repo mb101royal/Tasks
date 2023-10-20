f9bf85c83c952ba1cb9e37a14d49ca97cae9240b
mb101royal <mi8cugw8y@code.edu.az>
2023-10-20 05:06:57 +0400

    Completed thursday task.

---
+++ week_3_tasks/thursday_task/classes/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace classes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			User myUser1 = new User("Ebulfez", "ne kod?"); // 
+
+			myUser1.Login("Ebulfez", "ne od?"); // Sehv password yazib login edirik -> fail.
+
+			myUser1.Login("Ebulfez", "ne kod?"); // Duzgun melumatlarla daxil oluruq.
+
+			myUser1.Login("Ebulfez", "ne kod?"); // Hesaba login olmusuq ve yeniden olmaga calisdigimizi deyir.
+
+			myUser1.Logout(); // Hesabdan cixis etdik.
+
+			myUser1.Logout();
+		}
+	}
+}
---
+++ week_3_tasks/thursday_task/classes/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+	internal class User
+	{
+		public string Username;
+		public string Password;
+		public bool IsLoggedIn;
+
+		public User(string username, string password)
+		{
+			Username = username;
+			Password = password;
+			IsLoggedIn = false;
+		}
+
+		public void Login(string username, string password)
+		{
+			if (IsLoggedIn) Console.WriteLine("Istifadeci onceden hesaba daxil olub.");
+			else if (username == Username && password == Password)
+			{
+				IsLoggedIn = true;
+				Console.WriteLine("Istifadeci hesaba daxil oldu.");
+			}
+			else Console.WriteLine("Invalid username ve ya password. Yeniden cehd edin.");
+		}
+		public void Logout()
+		{
+			if (IsLoggedIn)
+			{
+				IsLoggedIn = false;
+				Console.WriteLine("Istifadeci hesabdan cixis etdi.");
+			}
+			else Console.WriteLine("Istifadeci hesaba giris etmeyib.");
+		}
+	}
+}
