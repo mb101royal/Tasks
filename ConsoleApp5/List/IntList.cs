60234d86a6b8c168b3180c6e3977cd156cbd85e2
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-24 13:57:18 +0400

    Add files via upload

---
+++ ConsoleApp5/List/IntList.cs
@@ -0,0 +1,39 @@
+﻿namespace List;
+
+internal class IntList
+{
+    private int[] _numbers;
+
+    public int[] Numbers { get => _numbers; set { } }
+
+
+    public void Add(int number)
+    {
+        Array.Resize(ref _numbers, _numbers.Length + 1);
+        Numbers[_numbers.Length - 1] = number;
+    }
+
+    public void Add(params int[] numbers)
+    {
+        Array.Resize(ref _numbers, _numbers.Length + numbers.Length);
+        Numbers[_numbers.Length - 1] = numbers;
+    }
+
+    public int Get(int index)
+    {
+        for (int i = 0; i < Numbers.Length; i++)
+        {
+            if (Numbers[i] == index) break;
+            index++;
+        }
+        return index;
+    }
+    public int[] GetAll()
+    {
+        return Numbers;
+    }
+    public void Print()
+    {
+        foreach (int i in Numbers) Console.Write(Numbers[i] + " ");
+    }
+}
