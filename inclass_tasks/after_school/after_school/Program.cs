aec249e4eb423d16eb3af4bf9c33179b528c3b4c
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-26 13:52:58 +0400

    Add files via upload

---
+++ inclass_tasks/after_school/after_school/Program.cs
@@ -0,0 +1,22 @@
+﻿using after_school.Records;
+
+namespace after_school
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            (string name, string username, byte age) person = ("Tuple", "Created", 0);
+            Console.WriteLine("{0} {1} {2}", person.Item1, person.Item2, person.Item3);
+
+            var tuple2 = new Tuple<string,string,byte>("Tuple2","Created",2);
+            Console.WriteLine("{0} {1} {2}", tuple2.Item1, tuple2.Item2, tuple2.Item3);
+
+            var tuple3 = student.GetStudent();
+
+            Console.WriteLine(tuple3);
+        }
+    }
+}
---
+++ inclass_tasks/after_school/after_school/Records/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace after_school.Records;
+
+internal record Student
+{
+    public string Name { get; init; } = "Celil";
+    public string Surname { get; init; } = "Celilov";
+    public byte Age { get; init; } = 10;
+
+    public Student()
+    {
+        Console.WriteLine("Normal: "+ Name + " " + Surname + " " + Age);
+        Console.WriteLine($"With Formatted string: {Name} {Surname} {Age}\n");
+    }
+
+    public Tuple<string, string, byte> GetStudent()
+    {
+        return Tuple.Create(Name, Surname, (byte)Age);
+    }
+}
