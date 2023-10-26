bc9acf92ce7577b391fda445c2af023e8bd5f57b
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-26 10:51:52 +0400

    Add files via upload

---
+++ inclass_tasks/school/school/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿namespace school.Models;
+
+internal class Group
+{
+    private int _studentCount;
+    private string _faculty;
+
+    public int StudentCount { get => _studentCount; set { _studentCount = value; } }
+    public string Faculty { get => _faculty; set { _faculty = value; } }
+
+    /*public Group(int count, string name)
+    {
+        StudentCount = count;
+        Faculty = name;
+    }*/
+}
---
+++ inclass_tasks/school/school/Models/Result.cs
@@ -0,0 +1,10 @@
+﻿namespace school.Models;
+
+internal class Result
+{
+    private Lesson _lesson;
+    private float _grade;
+
+    public Lesson Lesson { get => _lesson; set { _lesson = value; } }
+    public float Grade { get => _grade; set { _grade = value; } }
+}
