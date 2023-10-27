dbf2d7ddf5e8cab9ec5cd1811299e261096dc345
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-27 10:51:05 +0400

    Add files via upload

---
+++ inclass_tasks/abstrakt/abstrakt/Square.cs
@@ -0,0 +1,17 @@
+﻿namespace abstrakt
+{
+    internal class Square : Fiqure
+    {
+        private int _side;
+        public int Side { get => _side; set { if (value >= 0) _side = value; else Console.WriteLine("Menfi eded olmaz."); } }
+
+        public Square(int side)
+        {
+            Side = side;
+        }
+        public override int CalcArea()
+        {
+            return Side*Side;
+        }
+    }
+}
