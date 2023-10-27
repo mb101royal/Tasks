18984e1dc9cfe09d7dd9af4fec2343f23202b620
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-27 13:37:54 +0400

    Add files via upload

---
+++ inclass_tasks/animal_abstract/animal_abstract/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace animal_abstract;
+
+internal abstract class Animal
+{
+    public virtual void Eat()
+    {
+
+    }
+}
---
+++ inclass_tasks/animal_abstract/animal_abstract/Fish.cs
@@ -0,0 +1,14 @@
+﻿namespace animal_abstract;
+
+internal abstract class Fish : Animal
+{
+    public void Swim()
+    {
+        Console.WriteLine("Swam.");
+    }
+
+    public override void Eat()
+    {
+        Console.WriteLine("Fish is eating");
+    }
+}
