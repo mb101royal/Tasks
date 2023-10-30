c053657ab9c52524661c0140cd6dacfde9029b99
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-30 13:16:44 +0400

    Add files via upload

---
+++ inclass_tasks/products/products/Library.cs
@@ -0,0 +1,34 @@
+﻿namespace products;
+internal class Library
+{
+    Book[] _books = new Book[0];
+
+    public void AddBook(Book book)
+    {
+        Array.Resize(ref _books, _books.Length + 1);
+        _books[_books.Length - 1] = book;
+    }
+
+    public string GetBookById(int id)
+    {
+        if (id >= 0 && id < _books.Length - 1)
+            foreach (var book in _books)
+            {
+                if (id == book.Id)
+                {
+                    return book.ShowInfo();
+                } 
+            }
+        return "Invalid index.";
+    }
+    public void ShowAllBooks()
+    {
+        if (_books.Length > 0)
+
+            foreach (var book in _books)
+            {
+                Console.WriteLine(book.ShowInfo());
+            }
+        else Console.WriteLine("Kitabxana bosdur.");
+    }
+}
