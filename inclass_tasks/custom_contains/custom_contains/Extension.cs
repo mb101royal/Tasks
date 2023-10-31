91b9966f6545cdcf66156cd95a2cb18b2b4b7adf
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-10-31 12:00:35 +0400

    Add files via upload

---
+++ inclass_tasks/custom_contains/custom_contains/Extension.cs
@@ -0,0 +1,26 @@
+﻿namespace custom_contains
+{
+    internal static class Extension
+    {
+        public static bool CustomContains(this string userInput, string target)
+        {
+            userInput = userInput.Trim().ToLower();
+            target = target.Trim().ToLower();
+            bool result = false;
+            
+            for (int i = 0; i < userInput.Length;)
+            {
+                
+                if (userInput.Substring(i,  target.Length) == target)
+                {
+                    result = true;
+                    break;
+                }
+                if (i < userInput.Length - target.Length) i += 1;
+                else break;
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ inclass_tasks/custom_contains/custom_contains/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace custom_contains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bu bir custom contains methodudur.";
+
+            Console.WriteLine(name.CustomContains("Bu"));
+            Console.WriteLine(name.CustomContains("bu"));
+            Console.WriteLine(name.CustomContains("tains"));
+            Console.WriteLine(name.CustomContains("ns Methodudur"));
+            Console.WriteLine(name.CustomContains("bu bir custom"));
+        }
+    }
+}
