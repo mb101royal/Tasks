c6f01931fc0642aedf64da372f38ef4c8fbd185d
mb101royal <146910611+mb101royal@users.noreply.github.com>
2023-11-01 10:56:56 +0400

    Add files via upload

---
+++ inclass_tasks/repeat/Helper/CustomMethods.cs
@@ -0,0 +1,20 @@
+﻿namespace repeat.Helper
+{
+    public class CustomMethods
+    {
+        public bool CustomIsDigit(string value)
+        {
+            bool result = false;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsDigit(value[i]))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ inclass_tasks/repeat/Models/Authorization.cs
@@ -0,0 +1,52 @@
+﻿using Core.Models;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace repeat.Models
+{
+    public class Authorization
+    {
+        List<User> users = new List<User>();
+        public bool IsLoggedIn { get; set; }
+        public void Register(string name, string surname, ref string Username, ref string Password)
+        {
+            User user = new User();
+            user.Name = name;
+            user.Surname = surname;
+            user.Username = Username;
+            user.Password = Password;
+
+            users.Add(user);
+            IsLoggedIn = true;
+
+            Console.WriteLine("User registered.");
+        }
+        public void Login(string Username, string Password)
+        {
+            /*foreach (var user in users)
+            {
+                if (user.Username == Username && user.Password == Password)
+                {
+                    IsLoggedIn = true;
+                    Console.WriteLine("You're logged in.");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }*/
+        }
+        public void Logout()
+        {
+            if (IsLoggedIn)
+            {
+                IsLoggedIn = false;
+                Console.WriteLine("You're logged out.");
+            }
+            else
+            {
+                Console.WriteLine("You're not logged in.");
+            }
+        }
+    }
+}
